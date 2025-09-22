c0e98ea611e579139db8a39eb934617a6e92cfd9
zahramhld <139510503+zahramhld@users.noreply.github.com>
2025-09-22 23:05:03 +0330

    Update forms.py

--- user/forms.py
+++ user/forms.py
@@ -1,20 +1,17 @@
 from django import forms
-from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
 
-sex_choices = ((True, "female"), (False, "male"))
+User = get_user_model()
 
-class User(AbstractUser):
-    phone_number = models.CharField(max_length=11, null=True, blank=True)
-    location = models.CharField(max_length=255, null=True, blank=True)
-    sex = models.BooleanField(choices=sex_choices, null=True, blank=True)
-    
+class LoginForm(AuthenticationForm):
+    roles = (("doctor", "دکتر"), ("patient", "بیمار"))
+    role = forms.ChoiceField(choices=roles, label="نقش")
+
+class UserForm(forms.ModelForm):
     class Meta:
-        db_table = "user"
-
-    def __str__(self):
-        return self.username
-
+        model = User
+        fields = ["email", "first_name", "last_name", "phone_number", "location", "sex"]
 
 class EmailForm(forms.Form):
     email = forms.EmailField(label="ایمیل", max_length=255)
