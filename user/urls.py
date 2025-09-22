cb838f444625c2dc19445d34c4b490375612a7bf
zahramhld <139510503+zahramhld@users.noreply.github.com>
2025-09-22 23:50:55 +0330

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from . import views
 
+app_name = "user"
+
 urlpatterns = [
-    path('login/email/', views.send_email_otp, name='send_email_otp'),
-    path('login/email/verify/', views.verify_email_otp, name='verify_email_otp'),
+    path("signup/",  views.signup,      name="signup"),
+    path("login/",   views.login_view,  name="login"),
+    path("logout/",  views.logout_view, name="logout"),
+    path("profile/", views.profile,     name="profile"),
+    path("update/",  views.update,      name="update"),
+    path("delete/", views.delete_account, name="delete_account"),
+    
+
+    path("login/email/", views.send_email_otp, name="send_email_otp"),
+    path("login/email/verify/", views.verify_email_otp, name="verify_email_otp"),
 ]
