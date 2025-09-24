f1888ee4acc6edc652dc1fc540e41db81f43943c
zahramhld <139510503+zahramhld@users.noreply.github.com>
2025-09-24 15:07:09 +0330

    Update forms.py

--- user/forms.py
+++ user/forms.py
@@ -1,17 +1,25 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class SelectRoleForm(forms.Form):
+    ROLE_CHOICES = (("doctor", "دکتر"), ("patient", "بیمار"))
+    role = forms.ChoiceField(choices=ROLE_CHOICES, label="نقش", required=True)
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "email"]
+
 class LoginForm(AuthenticationForm):
-    roles = (("doctor", "دکتر"), ("patient", "بیمار"))
-    role = forms.ChoiceField(choices=roles, label="نقش")
+    pass
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ["email", "first_name", "last_name", "phone_number", "location", "sex"]
+        fields = ["first_name", "last_name", "email", "phone_number", "location", "sex"]
 
 class EmailForm(forms.Form):
     email = forms.EmailField(label="ایمیل", max_length=255)
